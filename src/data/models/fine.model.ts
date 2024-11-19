38ff696484cbb2f3ba1a15be666bfec56a5e3621
jxtsy <aj8984524@gmail.com>
2024-11-19 15:44:41 -0600

    Create fine model

---
+++ src/data/models/fine.model.ts
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+const fineSchema = new mongoose.Schema({
+  plate: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  speed: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  limit: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  lat: {
+    type: Number,
+    required: true,
+    min: -90,
+    max: 90,
+  },
+  lng: {
+    type: Number,
+    required: true,
+    min: -180,
+    max: 180,
+  },
+  isEmailSent: {
+    type: Boolean,
+    default: false,
+  },
+  creationDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const FineModel = mongoose.model('Fine', fineSchema);
