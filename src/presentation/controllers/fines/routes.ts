8477a95de36aadfec33fb9e1be93b0f55f461f75
Iván Campos <ivan.campos@cbqasolutions.com>
2024-11-24 21:05:12 -0600

    feature: created delete fine by id endpoint (#13)

--- src/presentation/controllers/fines/routes.ts
+++ src/presentation/controllers/fines/routes.ts
@@ -1,19 +1,17 @@
 import { Router } from "express";
 import { FineController } from "./controllers";
 
+export class FineRoutes {
+  static get routes(): Router {
+    const router = Router();
+    const controller = new FineController();
+    router.get("/", controller.getFines);
+    router.get("/plate/:plateNumber", controller.getFinesByPlateNumber);
+    router.get("/:id", controller.getFineById);
+    router.post("/", controller.createFine);
+    router.put("/:id", controller.updateFine);
+    router.delete("/:id", controller.deleteFineById);
 
-
-export class FineRoutes{
-    static get routes(): Router {
-        const router = Router();
-        const controller = new FineController();
-        router.get("/", controller.getFines)
-        router.get("/plate/:plateNumber", controller.getFinesByPlateNumber);
-        router.get("/:id", controller.getFineById);
-        router.post("/", controller.createFine);
-        router.put("/:id", controller.updateFine);
-        
-        return router;
-
-    }
+    return router;
+  }
 }
